0fde7fd790986cc988b7e0fe7416acdc5920da5d
Mario Beltrán Alarcón <belco90@gmail.com>
2018-06-04 00:32:17 +0200

    [Lesson 7] Add CSRF token with memcache

--- handlers/topic.py
+++ handlers/topic.py
@@ -1,18 +1,38 @@
+import uuid
+
 from handlers.base import BaseHandler
-from google.appengine.api import users
+from google.appengine.api import memcache, users
 
 from models.topic import Topic
 
 
 class TopicAddHandler(BaseHandler):
     def get(self):
-        return self.render_template("topic_add.html")
+        user = users.get_current_user()
+
+        if not user:
+            return self.write("Please login before you're allowed to post a topic.")
+
+        csrf_token = str(uuid.uuid4())
+        memcache.add(key=csrf_token, value=user.email(), time=600)
+
+        context = {
+            "csrf_token": csrf_token,
+        }
+
+        return self.render_template("topic_add.html", params=context)
 
     def post(self):
         user = users.get_current_user()
 
         if not user:
             return self.write("Please login before you're allowed to post a topic.")
+
+        csrf_token = self.request.get("csrf_token")
+        mem_token = memcache.get(key=csrf_token)
+
+        if not mem_token or mem_token != user.email():
+            return self.write("CSRF Token is protecting this website :)")
 
         title = self.request.get("title")
         text = self.request.get("text")
