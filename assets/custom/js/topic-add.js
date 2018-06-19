8c3608ebbfaa5ad0154acf28938b4cf202cc45d3
Mario Beltrán Alarcón <belco90@gmail.com>
2018-06-19 23:54:53 +0200

    [Homework 11.1] Check sum, show error and keep hiding button before submit

--- assets/custom/js/topic-add.js
+++ assets/custom/js/topic-add.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
   var $addTopicButton = $('#add-topic-button');
+  var $addTopicTitle = $('#add-topic-title');
+  var $addTopicText = $('#add-topic-text');
+  var $addTopicSum = $('#add-topic-sum');
 
-  $addTopicButton.on('click', function () {
-    $addTopicButton.addClass('hidden');
+  $addTopicButton.on('click', function (event) {
+    var error = false;
+
+    if ($addTopicSum.val() !== '10') {
+      error = true;
+      event.preventDefault();
+      $addTopicSum.closest('.form-group').addClass('has-error has-feedback');
+      $addTopicSum.siblings('.form-control-feedback, .help-block').removeClass('hidden');
+    } else {
+      $addTopicSum.closest('.form-group').removeClass('has-error has-feedback');
+      $addTopicSum.siblings('.form-control-feedback, .help-block').addClass('hidden');
+    }
+
+    if ($addTopicText.val() === '' || $addTopicTitle.val() === '') {
+      error = true;
+    }
+
+    if (!error) {
+      $addTopicButton.addClass('hidden');
+    }
+
   });
 });
